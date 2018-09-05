10502dc04558d9ffe59f68421dccc99862dbcc58
Sohel Rana <srana@leapinglogic.com>
2018-09-05 15:34:15 +0600

    update

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,5 +1,23 @@
 console.log('Before');
-setTimeout(() => {
-    console.log('timeout fn')
-}, 2000);
+ getUser(1, function(user) {
+    console.log('User', user);
+    getRepositories('sohel', (repo) => {
+        console.log('Repos : ',repo);
+    })
+});
 console.log('After');
+
+function getUser(id, callback){
+    setTimeout(() => {
+        console.log('Get a user from database.');
+        callback({id : 1, name:"sohel"});
+    }, 2000);
+}
+
+function getRepositories (username,callback){
+    setTimeout(() => {
+        console.log('Get a user from github.');
+        callback(['repo1', 'repo2']);     
+    }, 2000);
+  
+}
