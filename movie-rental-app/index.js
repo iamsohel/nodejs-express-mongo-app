7eb6215125ca31e7e7d985715f76dbe98f4e22bf
Sohel Rana <srana@leapinglogic.com>
2018-09-09 12:14:07 +0600

    project code update

--- movie-rental-app/index.js
+++ movie-rental-app/index.js
@@ -1,5 +1,6 @@
-const Joi = require('joi');
+
 const genres = require('./routes/genres');
+const customers = require('./routes/customers');
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use('/api/genres', genres);
+app.use('/api/customers', customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
