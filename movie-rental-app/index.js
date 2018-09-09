fb04a7ae9cccbcd1075f816b6e46914ff594f45f
Sohel Rana <srana@leapinglogic.com>
2018-09-09 17:32:45 +0600

    fawn

--- movie-rental-app/index.js
+++ movie-rental-app/index.js
@@ -1,17 +1,21 @@
-
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 
- mongoose.connect('mongodb://localhost/movie-rent')
- .then( () => console.log('Connected to Mongodb..'))
- .catch ( err => console.log('Could not connected to Mongodb'));
+mongoose.connect('mongodb://localhost/movie-rent')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
+
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
